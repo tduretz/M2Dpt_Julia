e57fd31224d0d8de5a2f542e5ca8fb0664ed5457
tduretz <imac@ar051018.spm.univ-rennes1.fr>
2020-02-17 11:47:45 +0100

    Test WENO MATLAB

--- Advection/TestAdvectionJulia.m
+++ Advection/TestAdvectionJulia.m
@@ -1,6 +1,10 @@
 clear
 
-upwind = 0;
+WENO5    = 1;
+test     = 0;
+vstep    = 5;
+order    = 2;
+dimsplit = 1;
 
 xmin     = -0.5;  xmax    =    0.5; Lx = xmax - xmin;
 ymin     = -0.5;  ymax    =    0.5; Ly = ymax - ymin;
@@ -8,6 +12,8 @@ zmin     = -0.50; zmax    =    0.5; Lz = zmax - zmin;
 nx       = 4*32;
 ny       = 4*32;
 nz       = 1;
+nt       = 300;
+nout     = 20;
 dx       = Lx/nx;
 dy       = Ly/ny;
 dz       = Lz/nz;
@@ -26,56 +32,67 @@ yv=ymin:dy:ymax;
 [xvx2,~,~] = ndgrid(xc,yv,zc);
 [~,yvy2,~] = ndgrid(xv,yc,zc);
 
-% Shift in x
-Vx      =  ones(nx+1,ny+0,nz+0);
-Vy      =  zeros(nx+0,ny+1,nz+0);
-Vz      =  zeros(nx+0,ny+0,nz+1);
+maxphiv = zeros(nt,1);
 
-% Shift in y
-Vx      =  zeros(nx+1,ny+0,nz+0);
-Vy      =   ones(nx+0,ny+1,nz+0);
-Vz      =  zeros(nx+0,ny+0,nz+1);
-
-% Rotate
-Vx      =   xvx2';
-Vy      =  -yvy2';
-Vz      =  zeros(nx+0,ny+0,nz+1);
-
-
-xC = 0.0;
-yC = 0.3;
-zC = 0.5*(zmin+zmax);
+if test==0
+    % Shift in x
+    xC = 0.0;
+    yC = 0.0;
+    zC = 0.5*(zmin+zmax);
+    Vx      =  ones(nx+1,ny+0,nz+0);
+    Vy      =  zeros(nx+0,ny+1,nz+0);
+    Vz      =  zeros(nx+0,ny+0,nz+1);
+elseif test==1
+    % Shift in y
+    xC = 0.0;
+    yC = 0.0;
+    zC = 0.5*(zmin+zmax);
+    Vx      =  zeros(nx+1,ny+0,nz+0);
+    Vy      =   ones(nx+0,ny+1,nz+0);
+    Vz      =  zeros(nx+0,ny+0,nz+1);
+elseif test==3
+    % Rotate
+    xC = 0.0;
+    yC = 0.3;
+    zC = 0.5*(zmin+zmax);
+    Vx      =   xvx2';
+    Vy      =  -yvy2';
+    Vz      =  zeros(nx+0,ny+0,nz+1);
+elseif test==4
+    % Shift in x y
+    xC = 0.0;
+    yC = 0.0;
+    zC = 0.5*(zmin+zmax);
+    Vx      =   ones(nx+1,ny+0,nz+0);
+    Vy      =   ones(nx+0,ny+1,nz+0);
+    Vz      =  zeros(nx+0,ny+0,nz+1);
+end
 
 % Tc_ex = exp(-(xce2-xC).^2/ 0.001 - (yce2-yC).^2/ 0.001 - (zce2-zC).^2/ 0.001);
 Tc_ex = exp(-(xce2-xC).^2/ 0.001 - (yce2-yC).^2/ 0.001);
 
-max(Tc_ex(:))
-
-vxm1   = min(Vx(1:end-1,:,int16(ceil(nz/2))),0);
-vxp1   = max(Vx(2:end  ,:,int16(ceil(nz/2))),0);
-vym1   = min(Vy(:,1:end-1,int16(ceil(nz/2))),0);
-vyp1   = max(Vy(:,2:end  ,int16(ceil(nz/2))),0);
-
+vxm1     = min(Vx(1:end-1,:,int16(ceil(nz/2))),0);
+vxp1     = max(Vx(2:end  ,:,int16(ceil(nz/2))),0);
+vym1     = min(Vy(:,1:end-1,int16(ceil(nz/2))),0);
+vyp1     = max(Vy(:,2:end  ,int16(ceil(nz/2))),0);
 phi1     = Tc_ex(2:end-1,2:end-1,int16(ceil(nz/2)));
-order    = 2;
-dimsplit = 1;
 
-VxC = 0.5*(Vx(1:end-1,:,:)+Vx(2:end-0,:,:));
-VyC = 0.5*(Vy(:,1:end-1,:)+Vy(:,2:end-0,:));
-vstep = 5;
+VxC   = 0.5*(Vx(1:end-1,:,:)+Vx(2:end-0,:,:));
+VyC   = 0.5*(Vy(:,1:end-1,:)+Vy(:,2:end-0,:));
 
-figure(1),clf;
-hold on
-imagesc(xc,yc, phi1' )
-colorbar
-quiver(xc(1:vstep:end,1:vstep:end),yc(1:vstep:end,1:vstep:end),VxC(1:vstep:end,1:vstep:end,1:vstep:end,1:vstep:end)',VyC(1:vstep:end,1:vstep:end,1:vstep:end,1:vstep:end)', 'w')
-set(gca,'ydir','normal')
-caxis([0 1])
-drawnow
+
+% figure(1),clf;
+% hold on
+% imagesc(xc,yc, phi1' )
+% colorbar
+% quiver(xc(1:vstep:end,1:vstep:end),yc(1:vstep:end,1:vstep:end),VxC(1:vstep:end,1:vstep:end,1:vstep:end,1:vstep:end)',VyC(1:vstep:end,1:vstep:end,1:vstep:end,1:vstep:end)', 'w')
+% set(gca,'ydir','normal')
+% caxis([0 1])
+% drawnow
 
 time = 0;
 
-for it=1:1000
+for it=1:nt
     
     time = time + dt;
     
@@ -83,11 +100,11 @@ for it=1:1000
     phi1o = phi1;
     phi1t = phi1;
     for step=1:order
-        if upwind == 1
-            %             phie  = [1*phi1t(end,:); phi1t; 1*phi1t(1,:)]; % periodic x
-            %             phie  = [1*phie(:,end)  phie  1*phie(:,1)];
-            phie  = [1*phi1t(1,:); phi1t; 1*phi1t(end,:)]; % no flux
-            phie  = [1*phie(:,1)  phie  1*phie(:,end)];
+        if WENO5 == 0
+            phie  = [1*phi1t(end,:); phi1t; 1*phi1t(1,:)]; % periodic x
+            phie  = [1*phie(:,end)  phie  1*phie(:,1)];
+            %             phie  = [1*phi1t(1,:); phi1t; 1*phi1t(end,:)]; % no flux
+            %             phie  = [1*phie(:,1)  phie  1*phie(:,end)];
             phxm1 = 1/dx*(phie(2:end-1,2:end-1) - phie(1:end-2,2:end-1));
             phxp1 = 1/dx*(phie(3:end,2:end-1)   - phie(2:end-1,2:end-1));
         else
@@ -98,7 +115,7 @@ for it=1:1000
     phi1t = (1/order)*phi1t + (1-1/order)*phi1o;
     phi1o = phi1t;
     for step=1:order
-        if upwind == 1
+        if WENO5 == 0
             phie  = [1*phi1t(1,:); phi1t; 1*phi1t(end,  :)]; % no flux
             phie  = [1*phie(:  ,1)  phie  1*phie(:,end)];
             phym1 = 1/dy*(phie(2:end-1,2:end-1) - phie(2:end-1,1:end-2));
@@ -110,11 +127,14 @@ for it=1:1000
     end
     phi1 = (1/order)*phi1t + (1-1/order)*phi1o;
     
+
+    
     %%%%%%%%%%%%%%%%%%%%%
     
     % centerx = time*1 + 0.1
+    maxphiv(it) = max(max(phi1(:)));
     
-    if mod(it,10)==0
+    if mod(it,nout)==0
         figure(1),clf;
         hold on
         imagesc(xc,yc, phi1' )
@@ -122,7 +142,9 @@ for it=1:1000
         quiver(xc(1:vstep:end,1:vstep:end),yc(1:vstep:end,1:vstep:end),VxC(1:vstep:end,1:vstep:end,1:vstep:end,1:vstep:end)',VyC(1:vstep:end,1:vstep:end,1:vstep:end,1:vstep:end)', 'w')
         set(gca,'ydir','normal')
         caxis([0 1])
-        title(max(max(phi1(:))))
+        title(maxphiv(it))
         drawnow
+        figure(2),clf;
+        plot(1:it, maxphiv(1:it), 'ok')
     end
 end
