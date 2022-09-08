b15a1f3514328471c80e9f98ff0af8f4071b631a
tduretz <imac@geo145.geophysik.uni-frankfurt.de>
2022-09-08 16:47:24 +0200

    one more step for Khaled

--- HT/1_ParallelStencil/HydroThermal3D_v8_Khaled.jl
+++ HT/1_ParallelStencil/HydroThermal3D_v8_Khaled.jl
@@ -15,13 +15,13 @@ else
     macro exp(args...)  esc(:(Base.exp($(args...)))) end
 end
 
-include("./tools/Macros.jl")  # Include macros - Cachemisère
-include("./tools/Weno5_Routines.jl")
-include("./kernels_HT3D.jl")
-
 using Printf, Statistics, LinearAlgebra, Plots
 # using HDF5
 using WriteVTK
+
+include("./tools/Macros.jl")  # Include macros - Cachemisère
+include("./tools/Weno5_Routines.jl")
+include("./kernels_HT3D.jl")
 
 ############################################## MAIN CODE ##############################################
 
@@ -38,213 +38,217 @@ using WriteVTK
     nout      = 10
     dt_fact   = 10
 
-    # ρf  =  1000*(1 - (0.00021*(T - 293)))
-    # ρs  =  2800*(1 - (0.000024*(T - 293)))
-    # Cps = 0.5915*(T) + 636.14
-    # Cpf = 1000 + 7.5*(T)
-    # kfc = 5e-15*exp((y - 500)/3000)
-    # kff = Perm*kc
-    # ktf = 0.0012*(T) - 0.069
-    # kts = 2.6842 - 0.0016*(T)
-    # kp  = 3.1138 - 0.0023*(T)  ?
-    # μf = 2.414*1e-5*10^(2.302*(247.8/(T - 140)))
-    # ϵp = 0.1
-    # θs = 1.0 - ϵp
+    # Characteristic dimensions
+    sc   = scaling()
+    sc.T = 500.0
+    sc.V = 1e-9
+    sc.L = 100e3
+    sc.η = 1e10
+    scale_me!( sc )
 
+    # kfc = 5e-15*exp((y - 500)/3000)                 OK
+    # kff = Perm*kc                                   OK
 
-# Physics
-xmin     = -0.0;  xmax = 120.0e3; Lx = xmax - xmin
-ymin     = -30e3; ymax = 0.0;     Ly = ymax - ymin
-zmin     = -0.00; zmax = 1.0e3;   Lz = zmax - zmin
-dT       = (ymax-ymin)*30e-3
-println(dT)
-Ttop     = 293.0
-Tbot     = Ttop + dT
-dP       = 0.25
-Ptop     = 0.0
-Pbot     = Ptop + dP
-T_i      = 10.0
-rho_cp   = 1.0
-lam0     = 1.0
-rad      = 1.0
-println(Ttop)
-println(Tbot)
-# Numerics
-fact     = 8 
-ncx      = fact*32-6
-ncy      = fact*8 -6
-ncz      = 3#fact*32-6
-# Preprocessing
-if (USE_MPI) me, dims, nprocs, coords, comm = init_global_grid(ncx, ncy, ncz; dimx=2, dimy=2, dimz=2);             
-else         me, dims, nprocs, coords       = (0, [1,1,1], 1, [0,0,0]);
-end
-Nix      = USE_MPI ? nx_g() : ncx                                                
-Niy      = USE_MPI ? ny_g() : ncy                                                
-Niz      = USE_MPI ? nz_g() : ncz                                                
-dx, dy, dz = Lx/Nix, Ly/Niy, Lz/Niz                                            
-dt       = min(dx,dy,dz)^2/6.1
-_dx, _dy, _dz = 1.0/dx, 1.0/dy, 1.0/dz
-_dt      = 1.0/dt
-# PT iteration parameters
-nitmax  = 1e4
-nitout  = 1000
-# Thermal solver
-tolT    = 1e-8
-tetT    = 0.1
-dtauT   = tetT*min(dx,dy,dz)^2/4.1
-# Darcy solver
-tolP     = 1e-10
-tetP     = 1/4/3
-dtauP    = tetP/6.1*min(dx,dy,dz)^2
-Pdamp    = 1.25
-dampx    = 1*(1-Pdamp/min(ncx,ncy,ncz))
-@printf("Go go!!\n")
+    # Physics
+    xmin     = -0.0/sc.L;  xmax = 120.0e3/sc.L; Lx = xmax - xmin
+    ymin     = -30e3/sc.L; ymax = 0.0/sc.L;     Ly = ymax - ymin
+    zmin     = -0.00/sc.L; zmax = 1.0e3/sc.L;   Lz = zmax - zmin
+    dT       = (ymax-ymin)*(30e-3/(sc.T/sc.L))
+    Ttop     = 293.0/sc.T
+    Tbot     = Ttop + dT
+    dP       = 0.25/sc.σ
+    Ptop     = 0.0/sc.σ
+    Pbot     = Ptop + dP
+    # T_i      = 10.0
+    # rho_cp   = 1.0
+    # lam0     = 1.0
+    # rad      = 1.0
+    Perm     = 50.0
+    ϵp       = 0.1
+    θs       = 1.0 - ϵp
 
-# Initialisation
-Told     = @zeros(ncx+0,ncy+0,ncz+0)
-v1       = @zeros(ncx+0,ncy+0,ncz+0)
-v2       = @zeros(ncx+0,ncy+0,ncz+0)
-v3       = @zeros(ncx+0,ncy+0,ncz+0)
-v4       = @zeros(ncx+0,ncy+0,ncz+0)
-v5       = @zeros(ncx+0,ncy+0,ncz+0)
-dTdxp    = @zeros(ncx+0,ncy+0,ncz+0)
-dTdxm    = @zeros(ncx+0,ncy+0,ncz+0)
-Tc       = @zeros(ncx+0,ncy+0,ncz+0)
-Tc_ex    = @zeros(ncx+2,ncy+2,ncz+2)
-Tc_exxx  = @zeros(ncx+6,ncy+6,ncz+6)
-Pc_ex    = @zeros(ncx+2,ncy+2,ncz+2)
-Ty       =  @ones(ncx  ,ncy+1,ncz  )
-ktv      =  @ones(ncx+1,ncy+1,ncz+1)
-kfv      =  @ones(ncx+1,ncy+1,ncz+1)
-dTdy     = @zeros(ncx  ,ncy  ,ncz  )
-Rt       = @zeros(ncx  ,ncy  ,ncz  )
-Rp       = @zeros(ncx  ,ncy  ,ncz  )
-kx       = @zeros(ncx+1,ncy  ,ncz  )
-ky       = @zeros(ncx  ,ncy+1,ncz  )
-kz       = @zeros(ncx  ,ncy  ,ncz+1)
-qx       = @zeros(ncx+1,ncy  ,ncz  )
-qy       = @zeros(ncx  ,ncy+1,ncz  )
-qz       = @zeros(ncx  ,ncy  ,ncz+1)
-Vx       = @zeros(ncx+1,ncy  ,ncz  )
-Vy       = @zeros(ncx  ,ncy+1,ncz  )
-Vz       = @zeros(ncx  ,ncy  ,ncz+1)
-Ft       = @zeros(ncx  ,ncy  ,ncz  )
-Ft0      = @zeros(ncx  ,ncy  ,ncz  )
-Vxm      = @zeros(ncx+0,ncy+0,ncz+0)
-Vxp      = @zeros(ncx+0,ncy+0,ncz+0)
-Vym      = @zeros(ncx+0,ncy+0,ncz+0)
-Vyp      = @zeros(ncx+0,ncy+0,ncz+0)
-Vzm      = @zeros(ncx+0,ncy+0,ncz+0)
-Vzp      = @zeros(ncx+0,ncy+0,ncz+0)
+    @printf("Surface T = %03f, bottom T = %03f\n", Ttop*sc.T, Tbot*sc.T)
 
-@printf("Memory was allocated!\n")
+    # Numerics
+    fact     = 8 
+    ncx      = fact*32-6
+    ncy      = fact*8 -6
+    ncz      = 3#fact*32-6
+    # Preprocessing
+    if (USE_MPI) me, dims, nprocs, coords, comm = init_global_grid(ncx, ncy, ncz; dimx=2, dimy=2, dimz=2);             
+    else         me, dims, nprocs, coords       = (0, [1,1,1], 1, [0,0,0]);
+    end
+    Nix      = USE_MPI ? nx_g() : ncx                                                
+    Niy      = USE_MPI ? ny_g() : ncy                                                
+    Niz      = USE_MPI ? nz_g() : ncz                                                
+    dx, dy, dz = Lx/Nix, Ly/Niy, Lz/Niz                                            
+    dt       = min(dx,dy,dz)^2/6.1
+    _dx, _dy, _dz = 1.0/dx, 1.0/dy, 1.0/dz
+    _dt      = 1.0/dt
+    # PT iteration parameters
+    nitmax  = 1e4
+    nitout  = 1000
+    # Thermal solver
+    tolT    = 1e-8
+    tetT    = 0.1
+    dtauT   = tetT*min(dx,dy,dz)^2/4.1
+    # Darcy solver
+    tolP     = 1e-10
+    tetP     = 1/4/3
+    dtauP    = tetP/6.1*min(dx,dy,dz)^2
+    Pdamp    = 1.25
+    dampx    = 1*(1-Pdamp/min(ncx,ncy,ncz))
+    @printf("Go go!!\n")
 
-# Pre-processing
-if USE_MPI
-    xc  = [x_g(ix,dx,Rt)+dx/2 for ix=1:ncx];
-    yc  = [y_g(iy,dy,Rt)+dy/2 for iy=1:ncy];
-    zc  = [z_g(iz,dz,Rt)+dz/2 for iz=1:ncz];
-    xce = [x_g(ix,dx,Rt)-dx/2 for ix=1:ncx+2]; # ACHTUNG
-    yce = [y_g(iy,dy,Rt)-dy/2 for iy=1:ncy+2]; # ACHTUNG
-    zce = [z_g(iz,dz,Rt)-dz/2 for iz=1:ncz+2]; # ACHTUNG
-    xv  = [x_g(ix,dx,Rt) for ix=1:ncx];
-    yv  = [y_g(iy,dy,Rt) for iy=1:ncy];
-    zv  = [z_g(iz,dz,Rt) for iz=1:ncz];
-    # Question 2 - how to xv
-else
-    xc  = LinRange(xmin+dx/2, xmax-dx/2, ncx)
-    yc  = LinRange(ymin+dy/2, ymax-dy/2, ncy)
-    zc  = LinRange(xmin+dz/2, zmax-dz/2, ncz)
-    xce = LinRange(xmin-dx/2, xmax+dx/2, ncx+2)
-    yce = LinRange(ymin-dy/2, ymax+dy/2, ncy+2)
-    zce = LinRange(xmin-dz/2, zmax+dz/2, ncz+2)
-    xv  = LinRange(xmin, xmax, ncx+1)
-    yv  = LinRange(ymin, ymax, ncy+1)
-    zv  = LinRange(xmin, zmax, ncz+1)
-end
-(xce2,yce2,zce2) = ([x for x=xce,y=yce,z=zce], [y for x=xce,y=yce,z=zce], [z for x=xce,y=yce,z=zce]) #SO: Replaced [Xc,Yc,Zc] = ndgrid(xc,yc,zc); because ndgrid does not exist. Again, this can be beautifully solved with array comprehensions.
-(xc2,yc2,zc2)    = ([x for x=xc,y=yc,z=zc],    [y for x=xc,y=yc,z=zc],    [z for x=xc,y=yc,z=zc]   ) #SO: Replaced [Xc,Yc,Zc] = ndgrid(xc,yc,zc); because ndgrid does not exist. Again, this can be beautifully solved with array comprehensions.
-(xv2,yv2,zv2)    = ([x for x=xv,y=yv,z=zv],    [y for x=xv,y=yv,z=zv],    [z for x=xv,y=yv,z=zv]   )
-@printf("Grid was set up!\n")
+    # Initialisation
+    Told     = @zeros(ncx+0,ncy+0,ncz+0)
+    v1       = @zeros(ncx+0,ncy+0,ncz+0)
+    v2       = @zeros(ncx+0,ncy+0,ncz+0)
+    v3       = @zeros(ncx+0,ncy+0,ncz+0)
+    v4       = @zeros(ncx+0,ncy+0,ncz+0)
+    v5       = @zeros(ncx+0,ncy+0,ncz+0)
+    dTdxp    = @zeros(ncx+0,ncy+0,ncz+0)
+    dTdxm    = @zeros(ncx+0,ncy+0,ncz+0)
+    Tc       = @zeros(ncx+0,ncy+0,ncz+0)
+    Tc_ex    = @zeros(ncx+2,ncy+2,ncz+2)
+    Tc_exxx  = @zeros(ncx+6,ncy+6,ncz+6)
+    Pc_ex    = @zeros(ncx+2,ncy+2,ncz+2)
+    Ty       =  @ones(ncx  ,ncy+1,ncz  )
+    kfv      =  @ones(ncx+1,ncy+1,ncz+1)
+    kts      =  @ones(ncx+1,ncy+1,ncz+1)
+    kff      =  @ones(ncx+1,ncy+1,ncz+1)
+    Cps      =  @ones(ncx+0,ncy+0,ncz+0)
+    Cpf      =  @ones(ncx+0,ncy+0,ncz+0)
+    μf       =  @ones(ncx+1,ncy+1,ncz+1)
+    dTdy     = @zeros(ncx  ,ncy  ,ncz  )
+    Rt       = @zeros(ncx  ,ncy  ,ncz  )
+    Rp       = @zeros(ncx  ,ncy  ,ncz  )
+    kx       = @zeros(ncx+1,ncy  ,ncz  )
+    ky       = @zeros(ncx  ,ncy+1,ncz  )
+    kz       = @zeros(ncx  ,ncy  ,ncz+1)
+    qx       = @zeros(ncx+1,ncy  ,ncz  )
+    qy       = @zeros(ncx  ,ncy+1,ncz  )
+    qz       = @zeros(ncx  ,ncy  ,ncz+1)
+    Vx       = @zeros(ncx+1,ncy  ,ncz  )
+    Vy       = @zeros(ncx  ,ncy+1,ncz  )
+    Vz       = @zeros(ncx  ,ncy  ,ncz+1)
+    Ft       = @zeros(ncx  ,ncy  ,ncz  )
+    Ft0      = @zeros(ncx  ,ncy  ,ncz  )
+    Vxm      = @zeros(ncx+0,ncy+0,ncz+0)
+    Vxp      = @zeros(ncx+0,ncy+0,ncz+0)
+    Vym      = @zeros(ncx+0,ncy+0,ncz+0)
+    Vyp      = @zeros(ncx+0,ncy+0,ncz+0)
+    Vzm      = @zeros(ncx+0,ncy+0,ncz+0)
+    Vzp      = @zeros(ncx+0,ncy+0,ncz+0)
 
-@time Tc_ex = Array(Tc_ex) # MAKE SURE ACTIVITY IS IN THE CPU:
-@time kfv   = Array(kfv)   # Ensure it is temporarily a CPU array
-@time SetInitialConditions(kfv, Tc_ex, xce2, yce2, zce2, xv2, yv2, zv2, yc2, Tbot, Ttop, dT, xmax, ymax, zmax, Ly)
-@time Tc_ex = Data.Array(Tc_ex) # MAKE SURE ACTIVITY IS IN THE GPU
-@time kfv   = Data.Array(kfv)
+    @printf("Memory was allocated!\n")
 
-@printf("Initial conditions were set up!\n")
+    # Pre-processing
+    if USE_MPI
+        xc  = [x_g(ix,dx,Rt)+dx/2 for ix=1:ncx];
+        yc  = [y_g(iy,dy,Rt)+dy/2 for iy=1:ncy];
+        zc  = [z_g(iz,dz,Rt)+dz/2 for iz=1:ncz];
+        xce = [x_g(ix,dx,Rt)-dx/2 for ix=1:ncx+2]; # ACHTUNG
+        yce = [y_g(iy,dy,Rt)-dy/2 for iy=1:ncy+2]; # ACHTUNG
+        zce = [z_g(iz,dz,Rt)-dz/2 for iz=1:ncz+2]; # ACHTUNG
+        xv  = [x_g(ix,dx,Rt) for ix=1:ncx];
+        yv  = [y_g(iy,dy,Rt) for iy=1:ncy];
+        zv  = [z_g(iz,dz,Rt) for iz=1:ncz];
+        # Question 2 - how to xv
+    else
+        xc  = LinRange(xmin+dx/2, xmax-dx/2, ncx)
+        yc  = LinRange(ymin+dy/2, ymax-dy/2, ncy)
+        zc  = LinRange(xmin+dz/2, zmax-dz/2, ncz)
+        xce = LinRange(xmin-dx/2, xmax+dx/2, ncx+2)
+        yce = LinRange(ymin-dy/2, ymax+dy/2, ncy+2)
+        zce = LinRange(xmin-dz/2, zmax+dz/2, ncz+2)
+        xv  = LinRange(xmin, xmax, ncx+1)
+        yv  = LinRange(ymin, ymax, ncy+1)
+        zv  = LinRange(xmin, zmax, ncz+1)
+    end
+    (xce2,yce2,zce2) = ([x for x=xce,y=yce,z=zce], [y for x=xce,y=yce,z=zce], [z for x=xce,y=yce,z=zce]) #SO: Replaced [Xc,Yc,Zc] = ndgrid(xc,yc,zc); because ndgrid does not exist. Again, this can be beautifully solved with array comprehensions.
+    (xc2,yc2,zc2)    = ([x for x=xc,y=yc,z=zc],    [y for x=xc,y=yc,z=zc],    [z for x=xc,y=yc,z=zc]   ) #SO: Replaced [Xc,Yc,Zc] = ndgrid(xc,yc,zc); because ndgrid does not exist. Again, this can be beautifully solved with array comprehensions.
+    (xv2,yv2,zv2)    = ([x for x=xv,y=yv,z=zv],    [y for x=xv,y=yv,z=zv],    [z for x=xv,y=yv,z=zv]   )
+    @printf("Grid was set up!\n")
 
-# #Define kernel launch params (used only if USE_GPU set true).
-# cuthreads = (32, 8, 1 )
-# cublocks  = ( 1, 4, 32).*fact
+    @time Tc_ex = Array(Tc_ex) # MAKE SURE ACTIVITY IS IN THE CPU:
+    @time kfv   = Array(kfv)   # Ensure it is temporarily a CPU array
+    @time SetInitialConditions(kfv, Tc_ex, xce2, yce2, zce2, xv2, yv2, zv2, yc2, Tbot, Ttop, dT, xmax, ymax, zmax, Ly, Perm, sc)
+    @time Tc_ex = Data.Array(Tc_ex) # MAKE SURE ACTIVITY IS IN THE GPU
+    @time kfv   = Data.Array(kfv)
 
-# evol=[]; it1=0; time=0             #SO: added warmpup; added one call to tic(); toc(); to get them compiled (to be done differently later).
-# ## Action
-# for it = it1:nt
+    @printf("Initial conditions were set up!\n")
 
-#     @printf("Thermal solver\n");
-# 	@parallel ResetA!(Ft,Rt)
-# 	@parallel InitThermal!(Rt, kx, ky, kz, Tc_ex, ktv, _dt)
+    # #Define kernel launch params (used only if USE_GPU set true).
+    # cuthreads = (32, 8, 1 )
+    # cublocks  = ( 1, 4, 32).*fact
 
-#     for iter = 0:nitmax
-#         @parallel SetPressureBCs!(Tc_ex, Tbot, Ttop)
-#         @parallel ComputeFlux!(qx, qy, qz, kx, ky, kz, Tc_ex, _dx, _dy, _dz)
-#         @parallel UpdateT!(Ft, Tc_ex, Rt, qx, qy, qz, _dt, dtauT, _dx, _dy, _dz)
-#         if (USE_MPI) update_halo!(Tc_ex); end
-#         if mod(iter,nitout) == 1
-#             # nFt = norm(Ft)/sqrt(ncx*ncy*ncz) # Question 3 - how to norm with GPU and MPI
-#             nFt = mean_g(abs.(Ft[:]))/sqrt(ncx*ncy*ncz)
-#             if (me==0) @printf("PT iter. #%05d - || Ft || = %2.2e\n", iter, nFt) end
-#             if nFt<tolT break end
-#         end
-#     end
+    # evol=[]; it1=0; time=0             #SO: added warmpup; added one call to tic(); toc(); to get them compiled (to be done differently later).
+    # ## Action
+    # for it = it1:nt
 
-#     @printf("min(Rt)    = %02.4e - max(Rt)    = %02.4e\n", minimum_g(Rt),    maximum_g(Rt)    )
-#     @printf("min(Tc_ex) = %02.4e - max(Tc_ex) = %02.4e\n", minimum_g(Tc_ex), maximum_g(Tc_ex) )
+    #     @printf("Thermal solver\n");
+    # 	@parallel ResetA!(Ft,Rt)
+    # 	@parallel InitThermal!(Rt, kx, ky, kz, Tc_ex, ktv, _dt)
 
-#     @printf("Darcy solver\n");
-# 	@parallel ResetA!(Ft, Ft0)
-# 	@parallel InitDarcy!(Ty, kx, ky, kz, Tc_ex, kfv, _dt)
-#     @parallel Compute_Rp!(Rp, ky, Ty, Ra, _dy)
+    #     for iter = 0:nitmax
+    #         @parallel SetPressureBCs!(Tc_ex, Tbot, Ttop)
+    #         @parallel ComputeFlux!(qx, qy, qz, kx, ky, kz, Tc_ex, _dx, _dy, _dz)
+    #         @parallel UpdateT!(Ft, Tc_ex, Rt, qx, qy, qz, _dt, dtauT, _dx, _dy, _dz)
+    #         if (USE_MPI) update_halo!(Tc_ex); end
+    #         if mod(iter,nitout) == 1
+    #             # nFt = norm(Ft)/sqrt(ncx*ncy*ncz) # Question 3 - how to norm with GPU and MPI
+    #             nFt = mean_g(abs.(Ft[:]))/sqrt(ncx*ncy*ncz)
+    #             if (me==0) @printf("PT iter. #%05d - || Ft || = %2.2e\n", iter, nFt) end
+    #             if nFt<tolT break end
+    #         end
+    #     end
 
-#     for iter = 0:nitmax
-#         @parallel SetPressureBCs!(Pc_ex, Pbot, Ptop)
-#         @parallel ComputeFlux!(qx, qy, qz, kx, ky, kz, Pc_ex, _dx, _dy, _dz)
-#         @parallel ResidualDiffusion!(Ft, Rp, qx, qy, qz, _dx, _dy, _dz)
-#         @parallel UpdateP!(Ft0, Pc_ex, Ft, dampx, dtauP)
+    #     @printf("min(Rt)    = %02.4e - max(Rt)    = %02.4e\n", minimum_g(Rt),    maximum_g(Rt)    )
+    #     @printf("min(Tc_ex) = %02.4e - max(Tc_ex) = %02.4e\n", minimum_g(Tc_ex), maximum_g(Tc_ex) )
 
-#         if (USE_MPI) update_halo!(Pc_ex); end
-#         if mod(iter,nitout) == 1
-#             nFt = mean_g(abs.(Ft[:]))/sqrt(ncx*ncy*ncz)
-#             if (me==0) @printf("PT iter. #%05d - || Ft || = %2.2e\n", iter, nFt) end
-#             if nFt<tolP break end
-#         end
-#     end
+    #     @printf("Darcy solver\n");
+    # 	@parallel ResetA!(Ft, Ft0)
+    # 	@parallel InitDarcy!(Ty, kx, ky, kz, Tc_ex, kfv, _dt)
+    #     @parallel Compute_Rp!(Rp, ky, Ty, Ra, _dy)
 
-#     @printf("min(Rp)    = %02.4e - max(Rp)    = %02.4e\n", minimum_g(Rp),    maximum_g(Rp)    )
-#     @printf("min(Pc_ex) = %02.4e - max(Pc_ex) = %02.4e\n", minimum_g(Pc_ex), maximum_g(Pc_ex) )
+    #     for iter = 0:nitmax
+    #         @parallel SetPressureBCs!(Pc_ex, Pbot, Ptop)
+    #         @parallel ComputeFlux!(qx, qy, qz, kx, ky, kz, Pc_ex, _dx, _dy, _dz)
+    #         @parallel ResidualDiffusion!(Ft, Rp, qx, qy, qz, _dx, _dy, _dz)
+    #         @parallel UpdateP!(Ft0, Pc_ex, Ft, dampx, dtauP)
 
-#     time  = time + dt;
-#     @printf("\n-> it=%d, time=%.1e, dt=%.1e, \n", it, time, dt);
+    #         if (USE_MPI) update_halo!(Pc_ex); end
+    #         if mod(iter,nitout) == 1
+    #             nFt = mean_g(abs.(Ft[:]))/sqrt(ncx*ncy*ncz)
+    #             if (me==0) @printf("PT iter. #%05d - || Ft || = %2.2e\n", iter, nFt) end
+    #             if nFt<tolP break end
+    #         end
+    #     end
 
-#     #---------------------------------------------------------------------
-#     if Advection == 1
-#         AdvectWithWeno5( Tc, Tc_ex, Tc_exxx, Told, dTdxm, dTdxp, Vxm, Vxp, Vym, Vyp, Vzm, Vzp, Vx, Vy, Vz, v1, v2, v3, v4, v5, kx, ky, kz, dt, _dx, _dy, _dz, Ttop, Tbot, Pc_ex, Ty, Ra )
-#     end
+    #     @printf("min(Rp)    = %02.4e - max(Rp)    = %02.4e\n", minimum_g(Rp),    maximum_g(Rp)    )
+    #     @printf("min(Pc_ex) = %02.4e - max(Pc_ex) = %02.4e\n", minimum_g(Pc_ex), maximum_g(Pc_ex) )
 
-# 	# Set dt for next step
-# 	dt  = dt_fact*1.0/6.1*min(dx,dy,dz) / max( maximum_g(abs.(Vx)), maximum_g(abs.(Vy)), maximum_g(abs.(Vz)))
-# 	_dt = 1/dt
-# 	@printf("Time step = %2.2e s\n", dt)
+    #     time  = time + dt;
+    #     @printf("\n-> it=%d, time=%.1e, dt=%.1e, \n", it, time, dt);
+
+    #     #---------------------------------------------------------------------
+    #     if Advection == 1
+    #         AdvectWithWeno5( Tc, Tc_ex, Tc_exxx, Told, dTdxm, dTdxp, Vxm, Vxp, Vym, Vyp, Vzm, Vzp, Vx, Vy, Vz, v1, v2, v3, v4, v5, kx, ky, kz, dt, _dx, _dy, _dz, Ttop, Tbot, Pc_ex, Ty, Ra )
+    #     end
+
+    # 	# Set dt for next step
+    # 	dt  = dt_fact*1.0/6.1*min(dx,dy,dz) / max( maximum_g(abs.(Vx)), maximum_g(abs.(Vy)), maximum_g(abs.(Vz)))
+    # 	_dt = 1/dt
+    # 	@printf("Time step = %2.2e s\n", dt)
 
     #---------------------------------------------------------------------
 
     if (Vizu == 1)
         X = Tc_ex[2:end-1,2:end-1,2:end-1]
         # display( heatmap(xc, yc, transpose(X[:,:,Int(ceil(ncz/2))]),c=:viridis,aspect_ratio=1) );
-        # display( heatmap(xce/1e3, yce/1e3, Tc_ex[:,:,2]', c=:jet1, aspect_ratio=1) );
-        display( heatmap(xv/1e3, yv/1e3, kfv[:,:,1]', c=:jet1, aspect_ratio=1) );
+        display( heatmap(xce*sc.L/1e3, yce*sc.L/1e3, Tc_ex[:,:,2]'.*sc.T.-273.15, c=:jet1, aspect_ratio=1) );
+        # display( heatmap(xv*sc.L/1e3, yv*sc.L/1e3, log10.(kfv[:,:,1]'.*sc.kf), c=:jet1, aspect_ratio=1) );
         # display(  contourf(xc,yc,transpose(Ty[:,:,Int(ceil(ncz/2))])) ) # accede au sublot 111
         #quiver(x,y,(f,f))
         @printf("Imaged sliced at z index %d over ncx = %d, ncy = %d, ncz = %d\n", Int(ceil(ncz/2)), ncx, ncy, ncz)
--- HT/1_ParallelStencil/kernels_HT3D.jl
+++ HT/1_ParallelStencil/kernels_HT3D.jl
@@ -1,24 +1,78 @@
+############################################## Scaling ##############################################
+
+Base.@kwdef mutable struct scaling
+    T ::Union{Data.Number, Missing} = missing
+    V ::Union{Data.Number, Missing} = missing
+    L ::Union{Data.Number, Missing} = missing
+    η ::Union{Data.Number, Missing} = missing
+    t ::Union{Data.Number, Missing} = missing
+    a ::Union{Data.Number, Missing} = missing
+    ε ::Union{Data.Number, Missing} = missing
+    σ ::Union{Data.Number, Missing} = missing
+    m ::Union{Data.Number, Missing} = missing
+    ρ ::Union{Data.Number, Missing} = missing
+    F ::Union{Data.Number, Missing} = missing
+    J ::Union{Data.Number, Missing} = missing
+    W ::Union{Data.Number, Missing} = missing
+    C ::Union{Data.Number, Missing} = missing
+    kt::Union{Data.Number, Missing} = missing
+    kf::Union{Data.Number, Missing} = missing
+end
+
+function scale_me!( scale )
+    scale.t    = scale.L / scale.V;
+    scale.a    = scale.V / scale.t;
+    scale.ε    = 1.0 / scale.t;
+    scale.σ    = scale.η / scale.t
+    scale.m    = scale.σ * scale.L * scale.t^2
+    scale.ρ    = scale.m / scale.L^3
+    scale.F    = scale.m * scale.L / scale.t^2
+    scale.J    = scale.m * scale.L^2 / scale.t^2
+    scale.W    = scale.J / scale.t
+    scale.C    = scale.J / scale.m / scale.T
+    scale.kt   = scale.W / scale.L / scale.T
+    scale.kf   = scale.L^2
+    return nothing
+end
+
 ############################################## Kernels for HT code ##############################################
 
-@views function SetInitialConditions(kfv::Array{Float64,3}, Tc_ex::Array{Float64,3}, xce2::Array{Float64,3}, yce2::Array{Float64,3}, zce2::Array{Float64,3}, xv2::Array{Float64,3}, yv2::Array{Float64,3}, zv2::Array{Float64,3}, yc2::Array{Float64,3}, Tbot::Data.Number, Ttop::Data.Number, dT::Data.Number, xmax::Data.Number, ymax::Data.Number, zmax::Data.Number, Ly::Data.Number)
+@views function UpdateNonLinearity(ρs::Array{Float64,3}, ρf::Array{Float64,3}, kfv::Array{Float64,3}, Cps::Array{Float64,3}, Cpf::Array{Float64,3}, kts::Array{Float64,3}, ktf::Array{Float64,3}, Tc_ex::Array{Float64,3}, Perm::Data.Number, sc::scaling)
+    Tsca = T*sc.T 
+    @. ρf  = ( 1000*(1 - (0.000210*(Tsca - 293))) )/sc.ρ
+    @. ρs  = ( 2800*(1 - (0.000024*(Tsca - 293))) )/sc.ρ
+    @. Cps = ( 0.5915*(T) + 636.14 )/sc.C
+    @. Cpf = ( 1000 + 7.5*(T) )/sc.C
+    @. ktf = ( 0.0012*(T) - 0.069 )/sc.kf
+    @. kts = ( 2.6842 - 0.0016*(T) )/sc.kf
+    # @. kp  = ( 3.1138 - 0.0023*(T) )/sc.kt
+    @. μf  = ( 2.414*1e-5*10^(2.302*(247.8/(T - 140))) )/sc.η
+end
+
+@views function SetInitialConditions(kfv::Array{Float64,3}, Tc_ex::Array{Float64,3}, xce2::Array{Float64,3}, yce2::Array{Float64,3}, zce2::Array{Float64,3}, xv2::Array{Float64,3}, yv2::Array{Float64,3}, zv2::Array{Float64,3}, yc2::Array{Float64,3}, Tbot::Data.Number, Ttop::Data.Number, dT::Data.Number, xmax::Data.Number, ymax::Data.Number, zmax::Data.Number, Ly::Data.Number, Perm::Data.Number, sc::scaling)
     # @parallel function SetInitialConditions(kfv, Tc_ex, Tbot, Ttop, dT, xce2, yce2, xv2, yv2, Ly, yc2, xmax, ymax)
         # Initial conditions: Draw Fault
+        in_fault = zero(kfv)
         # top
-        x1 = 68466.18089117216; x2 = 31498.437753425103
-        y1 = 0.0;               y2 = -16897.187956165293
+        x1 = 68466.18089117216/sc.L; x2 = 31498.437753425103/sc.L
+        y1 = 0.0/sc.L;               y2 = -16897.187956165293/sc.L
         a1 = ( y1-y2 ) / ( x1-x2 )
         b1 = y1 - x1*a1
         # bottom
-        x1 = 32000.0; x2 = 69397.54576150524
-        y1 =-17800.0; y2 = 0.
+        x1 = 32000.0/sc.L; x2 = 69397.54576150524/sc.L
+        y1 =-17800.0/sc.L; y2 = 0.0/sc.L
         a2 = ( y1-y2 ) / ( x1-x2 )
         b2 = y1 - x1*a2
         # bottom
-        x1 = 32000.0; x2 = 31498.437753425103
-        y1 =-17800.0; y2 = -16897.187956165293
+        x1 = 32000.0/sc.L; x2 = 31498.437753425103/sc.L
+        y1 =-17800.0/sc.L; y2 = -16897.187956165293/sc.L
         a3 = ( y1-y2 ) / ( x1-x2 )
         b3 = y1 - x1*a3
-        @. kfv[ yv2 < (xv2*a1 + b1) && yv2 > (xv2*a2 + b2) && yv2 > (xv2*a3 + b3)  ] = 20
+        kf0 = 5e-15/sc.kf
+        y1  = 500.0/sc.L; y2 = 3000.0/sc.L
+        @. kfv = kf0*exp((yv2 - y1)/y2)
+        @. in_fault = yv2 < (xv2*a1 + b1) && yv2 > (xv2*a2 + b2) && yv2 > (xv2*a3 + b3)
+        @. kfv[ yv2 < (xv2*a1 + b1) && yv2 > (xv2*a2 + b2) && yv2 > (xv2*a3 + b3)  ] = Perm*kfv[ yv2 < (xv2*a1 + b1) && yv2 > (xv2*a2 + b2) && yv2 > (xv2*a3 + b3)  ]
         # Thermal field
         @. Tc_ex = Ttop - dT/Ly * yce2
         @. Tc_ex[1,:,:] =          Tc_ex[2,:,:]; @. Tc_ex[end,:,:] =          Tc_ex[end-1,:,:]
@@ -45,14 +99,14 @@ end
 	@all(ky) = @av_xza(kfv)
 	@all(kz) = @av_xya(kfv)
 
-	return
+	return nothing
 end
 
 @parallel function Compute_Rp!(Rp::Data.Array, ky::Data.Array, Ty::Data.Array, Ra::Data.Number, _dy::Data.Number)
 
     @all(Rp) = Ra*_dy*@dmul_ya(ky, Ty)
 
-    return
+    return nothing
 end
 
 @parallel function InitThermal!(Rt::Data.Array, kx::Data.Array, ky::Data.Array, kz::Data.Array, Tc_ex::Data.Array, ktv::Data.Array, _dt::Data.Number)
@@ -62,7 +116,7 @@ end
     @all(ky) = @av_xza(ktv)
     @all(kz) = @av_xya(ktv)
 
-    return
+    return nothing
 end
 
 @parallel function ComputeFlux!(qx::Data.Array, qy::Data.Array, qz::Data.Array, kx::Data.Array, ky::Data.Array, kz::Data.Array, A::Data.Array,
@@ -72,7 +126,7 @@ end
     @all(qy) = -@all(ky)*(_dy*@d_yi(A))
     @all(qz) = -@all(kz)*(_dz*@d_zi(A))
 
-    return
+    return nothing
 end
 
 @parallel function UpdateT!(F::Data.Array, T::Data.Array, R::Data.Array, qx::Data.Array, qy::Data.Array, qz::Data.Array, _dt::Data.Number, dtau::Data.Number,
@@ -81,7 +135,7 @@ end
     @all(F) = @all(R) + _dt*@inn(T) +  _dx*@d_xa(qx) + _dy*@d_ya(qy) + _dz*@d_za(qz)
     @inn(T) = @inn(T) - dtau * @all(F)
 
-    return
+    return nothing
 end
 
 @parallel function ResidualDiffusion!(F::Data.Array, R::Data.Array, qx::Data.Array, qy::Data.Array, qz::Data.Array,
@@ -89,7 +143,7 @@ end
 
 	@all(F) = @all(R) +  _dx*@d_xa(qx) + _dy*@d_ya(qy) + _dz*@d_za(qz)
 
-    return
+    return nothing
 end
 
 @parallel function UpdateP!(F0::Data.Array, P::Data.Array, F::Data.Array, dampx::Data.Number, dtau::Data.Number)
@@ -97,7 +151,7 @@ end
     @all(F0) = @all(F) + dampx*@all(F0)
     @inn(P ) = @inn(P) - dtau *@all(F0)
 
-    return
+    return nothing
 end
 
 @parallel_indices (ix,iy,iz) function SetPressureBCs!(Pc_ex::Data.Array, Pbot::Data.Number, Ptop::Data.Number)
@@ -109,7 +163,7 @@ end
     if (ix<=size(Pc_ex,1) && iy<=size(Pc_ex,2) && iz==1            ) Pc_ex[ix,iy,            1] =          Pc_ex[ix,iy,              2]  end
     if (ix<=size(Pc_ex,1) && iy<=size(Pc_ex,2) && iz==size(Pc_ex,3)) Pc_ex[ix,iy,size(Pc_ex,3)] =          Pc_ex[ix,iy,size(Pc_ex,3)-1]  end
 
-    return
+    return nothing
 end
 
 @views function AdvectWithWeno5( Tc, Tc_ex, Tc_exxx, Told, dTdxm, dTdxp, Vxm, Vxp, Vym, Vyp, Vzm, Vzp, Vx, Vy, Vz, v1, v2, v3, v4, v5, kx, ky, kz, dt, _dx, _dy, _dz, Ttop, Tbot, Pc_ex, Ty, Ra )
